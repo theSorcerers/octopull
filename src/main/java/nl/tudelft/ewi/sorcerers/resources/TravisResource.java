8b78fcc3c31787733789c3f9de0856f8ce3088c8
rmhartog <r.m.hartog@hotmail.com>
2015-03-21 12:46:40 +0100

    Updated json payload for travis

--- src/main/java/nl/tudelft/ewi/sorcerers/resources/TravisResource.java
+++ src/main/java/nl/tudelft/ewi/sorcerers/resources/TravisResource.java
@@ -1,28 +1,60 @@
 package nl.tudelft.ewi.sorcerers.resources;
 
+import java.io.IOException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 
 @Path("/travis")
 public class TravisResource {
-	@Context ObjectMapper mapper;
+
+	@GET
+	@Path("/webhook")
+	public Response test() {
+		return webhook("{\"commit\": \"123\" }");
+	}
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Path("/webhook")
 	public Response webhook(@FormParam("payload") String payload) {
 		System.out.println(payload);
-		System.out.println(mapper.convertValue(payload, TravisPayload.class));
+		
+		ObjectMapper mapper = new ObjectMapper();
+		AnnotationIntrospector introspector = new JacksonAnnotationIntrospector();
+		// make deserializer use JAXB annotations (only)
+		mapper.getDeserializationConfig().withAppendedAnnotationIntrospector(introspector);
+		// make serializer use JAXB annotations (only)
+		mapper.getSerializationConfig().withAppendedAnnotationIntrospector(introspector);
+		try {
+			System.out.println(mapper.readValue(payload, TravisPayload.class).commit);
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return Response.ok().build();
 	}
 	
+	@XmlRootElement
 	private static class TravisPayload {
 		public String commit;
 	}
