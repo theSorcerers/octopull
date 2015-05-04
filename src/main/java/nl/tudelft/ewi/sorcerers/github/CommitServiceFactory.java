c6265f1bfe8a135afb72dacbfb7e876a547cdd8d
rmhartog <r.m.hartog@hotmail.com>
2015-05-04 20:23:40 +0200

    Fix Commit Service provider

--- src/main/java/nl/tudelft/ewi/sorcerers/github/CommitServiceFactory.java
+++ src/main/java/nl/tudelft/ewi/sorcerers/github/CommitServiceFactory.java
@@ -1,22 +1,23 @@
 package nl.tudelft.ewi.sorcerers.github;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.CommitService;
 import org.glassfish.hk2.api.Factory;
 
 public class CommitServiceFactory implements Factory<CommitService> {
-	private GitHubClient client;
+	private Provider<GitHubClient> clientProvider;
 
 	@Inject
-	public CommitServiceFactory(GitHubClient client) {
-		this.client = client;
+	public CommitServiceFactory(Provider<GitHubClient> clientProvider) {
+		this.clientProvider = clientProvider;
 	}
 
 	@Override
 	public CommitService provide() {
-		return new CommitService(this.client);
+		return new CommitService(this.clientProvider.get());
 	}
 
 	@Override
