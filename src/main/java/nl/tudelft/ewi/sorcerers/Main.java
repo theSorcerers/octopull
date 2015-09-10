6c82dd1ab51f9e648d7c59736c1c0d7b2d024025
rmhartog <r.m.hartog@hotmail.com>
2015-09-10 10:34:19 +0200

    Use jetty 9 + threadpool to prevent blocking (504)

--- src/main/java/nl/tudelft/ewi/sorcerers/Main.java
+++ src/main/java/nl/tudelft/ewi/sorcerers/Main.java
@@ -2,10 +2,15 @@ package nl.tudelft.ewi.sorcerers;
 
 import static org.glassfish.jersey.CommonProperties.METAINF_SERVICES_LOOKUP_DISABLE;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ThreadPool;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 public class Main {
@@ -13,15 +18,23 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Starting server on port 8080");
 
-		// Create the server
-		Server server = new Server(8080);
 		
+		// Create the server
+		ThreadPool threadPool = new QueuedThreadPool(10, 4, 60, new ArrayBlockingQueue<Runnable>(6000));
+		Server server = new Server(threadPool);
+
+		ServerConnector connector = new ServerConnector(server);
+		connector.setPort(8080);
+		server.addConnector(connector);
+
 		// Create a servlet context and add the jersey servlet
 		ServletContextHandler sch = new ServletContextHandler(server, "/");
 
 		FilterHolder servletContainer = new FilterHolder(ServletContainer.class);
-		servletContainer.setInitParameter("javax.ws.rs.Application", AppConfig.class.getCanonicalName());
-		servletContainer.setInitParameter(METAINF_SERVICES_LOOKUP_DISABLE, "true");
+		servletContainer.setInitParameter("javax.ws.rs.Application",
+				AppConfig.class.getCanonicalName());
+		servletContainer.setInitParameter(METAINF_SERVICES_LOOKUP_DISABLE,
+				"true");
 		sch.addFilter(servletContainer, "/*", null);
 
 		// Must add DefaultServlet for embedded Jetty.
@@ -30,7 +43,7 @@ public class Main {
 
 		// Start the server
 		server.start();
-		
+
 		// Wait for server to shutdown
 		server.join();
 	}
