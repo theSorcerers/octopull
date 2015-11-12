c7b2b9fd433a46f4e2e43b140fdc3d08fc3eac68
rmhartog <r.m.hartog@hotmail.com>
2015-11-12 11:40:08 +0100

    Move URI retrieval to CommentService

--- src/main/java/nl/tudelft/ewi/sorcerers/model/CommentService.java
+++ src/main/java/nl/tudelft/ewi/sorcerers/model/CommentService.java
@@ -1,9 +1,12 @@
 package nl.tudelft.ewi.sorcerers.model;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Date;
 
 import javax.inject.Inject;
+
+import nl.tudelft.ewi.sorcerers.github.PatchedGitHubClient.PatchedCommitComment;
 
 import org.eclipse.egit.github.core.CommitComment;
 import org.eclipse.egit.github.core.RepositoryId;
@@ -23,30 +26,30 @@ public class CommentService {
 		this.warningCommentRepository = warningCommentRepository;
 	}
 
-	public CommitComment createCommentFromWarning(String repo, String commit,
+	public URI createCommentFromWarning(String repo, String commit,
 			Integer pullRequest, Integer warningId, Integer position)
 			throws IOException {
 		Warning warning = this.warningService.get(repo, commit, warningId);
-		if (warning == null) {
+		if (warning == null)
 			throw new IllegalArgumentException("No such warning exists.");
-		} else {
-			StringBuilder bodyBuilder = new StringBuilder();
-			bodyBuilder.append("*Auto-generated comment*\n\n");
-			bodyBuilder.append("    ");
-			bodyBuilder.append(warning.getMessage().replaceAll("\n", "\n    "));
-			bodyBuilder.append("\n");
+		
+		StringBuilder bodyBuilder = new StringBuilder();
+		bodyBuilder.append("*Auto-generated comment*\n\n");
+		bodyBuilder.append("    ");
+		bodyBuilder.append(warning.getMessage().replaceAll("\n", "\n    "));
+		bodyBuilder.append("\n");
 
-			CommitComment comment = new CommitComment();
-			comment.setBody(bodyBuilder.toString());
-			comment.setCommitId(commit);
-			comment.setPath(warning.getPath());
-			comment.setPosition(position);
-			comment = this.pullRequestService.createComment(
-					RepositoryId.createFromId(repo), pullRequest, comment);
+		CommitComment comment = new CommitComment();
+		comment.setBody(bodyBuilder.toString());
+		comment.setCommitId(commit);
+		comment.setPath(warning.getPath());
+		comment.setPosition(position);
+		PatchedCommitComment result = (PatchedCommitComment) this.pullRequestService.createComment(
+				RepositoryId.createFromId(repo), pullRequest, comment);
 
-			this.warningCommentRepository.add(new WarningComment(warning.getRepo(), warning.getCommit(), warning.getId(), new Date(), comment.getId()));
-			
-			return comment;
-		}
+		this.warningCommentRepository.add(new WarningComment(warning.getRepo(), warning.getCommit(), warning.getId(), new Date(), result.getId()));
+		
+		URI commentUri = URI.create(result.getHtmlUrl());
+		return commentUri;
 	}
 }
--- src/main/java/nl/tudelft/ewi/sorcerers/usecases/CreateCommentFromWarning.java
+++ src/main/java/nl/tudelft/ewi/sorcerers/usecases/CreateCommentFromWarning.java
@@ -1,12 +1,12 @@
 package nl.tudelft.ewi.sorcerers.usecases;
 
 import java.io.IOException;
+import java.net.URI;
 
 import javax.inject.Inject;
 
-import org.eclipse.egit.github.core.CommitComment;
-
 import nl.tudelft.ewi.sorcerers.model.CommentService;
+import nl.tudelft.ewi.sorcerers.resources.CommentResource;
 
 public class CreateCommentFromWarning {
 	private CommentService commentService;
@@ -16,7 +16,7 @@ public class CreateCommentFromWarning {
 		this.commentService = commentService;
 	}
 	
-	public CommitComment execute(String repo, String commit, Integer pullRequest, Integer warningId, Integer position) throws IOException {
+	public URI execute(CommentResource commentResource, String repo, String commit, Integer pullRequest, Integer warningId, Integer position) throws IOException {
 		return this.commentService.createCommentFromWarning(repo, commit, pullRequest, warningId, position);
 	}
 }
